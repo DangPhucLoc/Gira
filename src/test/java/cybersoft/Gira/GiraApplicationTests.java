a4897526ceba2b2ae0a07f114ab9e7230661ddd4
DangPhucLoc <LocDPSE171702@fpt.edu.vn>
2022-09-17 15:30:00 +0700

    Init commit

--- src/test/java/cybersoft/Gira/GiraApplicationTests.java
+++ src/test/java/cybersoft/Gira/GiraApplicationTests.java
@@ -2,12 +2,14 @@ package cybersoft.Gira;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class GiraApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(1 + 1).isEqualTo(2);
 	}
 
 }
